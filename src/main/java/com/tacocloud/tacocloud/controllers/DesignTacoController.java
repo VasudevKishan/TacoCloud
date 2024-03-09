1696f988a8f601bb822a6c227519f68cb7c94cde
Vasudev Kishan <vasudevkishan@gmail.com>
2024-03-10 00:08:00 +0530

    Setup Ingredient Repository (upto page 70)

--- src/main/java/com/tacocloud/tacocloud/controllers/DesignTacoController.java
+++ src/main/java/com/tacocloud/tacocloud/controllers/DesignTacoController.java
@@ -3,6 +3,9 @@ package com.tacocloud.tacocloud.controllers;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import lombok.extern.slf4j.Slf4j;
+
+import com.tacocloud.tacocloud.data.IngredientReporsitory;
 import com.tacocloud.tacocloud.model.Ingredient;
 import com.tacocloud.tacocloud.model.Ingredient.Type;
 
@@ -26,19 +31,30 @@ import com.tacocloud.tacocloud.model.TacoOrder;
 @RequestMapping("/design")
 @SessionAttributes("tacoOrder")
 public class DesignTacoController {
+
+  private final IngredientReporsitory ingredientRepo;
+
+  @Autowired
+  public DesignTacoController(IngredientReporsitory ingredientRepo) {
+    this.ingredientRepo = ingredientRepo;
+
+  }
+
   @ModelAttribute
   public void addIngredientsToModel(Model model) {
-    List<Ingredient> ingredients = Arrays.asList(
-        new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-        new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-        new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-        new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-        new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-        new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-        new Ingredient("CHED", "Cheddar", Type.CHEESE),
-        new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-        new Ingredient("SLSA", "Salsa", Type.SAUCE),
-        new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
+    // List<Ingredient> ingredients = Arrays.asList(
+    // new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
+    // new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
+    // new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
+    // new Ingredient("CARN", "Carnitas", Type.PROTEIN),
+    // new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
+    // new Ingredient("LETC", "Lettuce", Type.VEGGIES),
+    // new Ingredient("CHED", "Cheddar", Type.CHEESE),
+    // new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
+    // new Ingredient("SLSA", "Salsa", Type.SAUCE),
+    // new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
+
+    Iterable<Ingredient> ingredients = ingredientRepo.findAll();
     Type[] types = Ingredient.Type.values();
     for (Type type : types) {
       model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
@@ -72,10 +88,14 @@ public class DesignTacoController {
   }
 
   private Iterable<Ingredient> filterByType(
-      List<Ingredient> ingredients, Type type) {
-    return ingredients
-        .stream()
+      Iterable<Ingredient> ingredients, Type type) {
+    return StreamSupport.stream(ingredients.spliterator(), false)
         .filter(x -> x.getType().equals(type))
         .collect(Collectors.toList());
+
+    // (ingredients)
+    // .stream()
+    // .filter(x -> x.getType().equals(type))
+    // .collect(Collectors.toList());
   }
 }
---
+++ src/main/java/com/tacocloud/tacocloud/data/JdbcIngredientRepository.java
@@ -0,0 +1,53 @@
+package com.tacocloud.tacocloud.data;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.tacocloud.tacocloud.model.Ingredient;
+
+@Repository
+public class JdbcIngredientRepository implements IngredientReporsitory {
+
+  private JdbcTemplate jdbcTemplate;
+
+  @Autowired
+  public JdbcIngredientRepository(JdbcTemplate jdbcTemplate) {
+    this.jdbcTemplate = jdbcTemplate;
+  }
+
+  private Ingredient mapToRowIngredient(ResultSet row, int rowNum) throws SQLException {
+    return new Ingredient(
+        row.getString("id"),
+        row.getString("name"),
+        Ingredient.Type.valueOf(row.getString("type")));
+  }
+
+  @Override
+  public Iterable<Ingredient> findAll() {
+    return jdbcTemplate.query("select id, name, type from Ingredient", this::mapToRowIngredient);
+  }
+
+  @Override
+  public Optional<Ingredient> findById(String id) {
+    List<Ingredient> result = jdbcTemplate.query("select id, name, type from Ingredient where id = ?",
+        this::mapToRowIngredient, id);
+    return result.size() == 0 ? Optional.empty() : Optional.of(result.get(0));
+  }
+
+  @Override
+  public Ingredient save(Ingredient ingredient) {
+    jdbcTemplate.update("insert into Ingredient (id, name, type) values (? ,? ,?)",
+        ingredient.getId(),
+        ingredient.getName(),
+        ingredient.getType().toString());
+
+    return ingredient;
+  }
+
+}
